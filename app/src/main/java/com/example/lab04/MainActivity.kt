6f69841e4510b400aa5d392a2c4c37a185ee951d
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-08-26 22:23:12 -0500

    Add slider component

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainContent(
+                        name = "Sujenn",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,9 +30,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainContent(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Greeting(name = name)
+
+        // Componente Slider
+        var sliderPosition by remember { mutableStateOf(0f) }
+        Text(text = "Slider Value: ${sliderPosition.toInt()}")
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            valueRange = 0f..100f,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hola $name!",
         modifier = modifier
     )
 }
@@ -42,6 +63,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
-        Greeting("Android")
+        MainContent(name = "Sujenn")
     }
 }
